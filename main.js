b85657680b48853c9d6f0f022daff08fa7fa558f
nildinho <nildinho.anahi@hotmail.com>
2024-06-19 17:24:59 -0300

    nildinho

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const { ipcMain } = require('electron')
 const { app, BrowserWindow, Menu, shell } = require('electron/main')
 const path = require('node:path')
- 
+
 // Importar o módulo de conexão
 const { conectar, desconectar } = require('./database.js')
- 
- 
+
+
 // Janela Principal (definir o objeto win como variavel publica)
 let win
 const createWindow = () => {
-     win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 800,
         height: 600,
-        icon: './src/public/img/estoque.png',
+        icon: './src/public/img/estoquesobre.png',
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
         }
@@ -20,7 +20,7 @@ const createWindow = () => {
     Menu.setApplicationMenu(Menu.buildFromTemplate(template))
     win.loadFile('./src/views/index.html')
 }
- 
+
 //janela sobre
 let about //Resolver bug de abertura de várias janelas
 
@@ -33,7 +33,7 @@ const aboutWindow = () => {
             resizable: false, //evitar o redimensionamento
             //titleBarStyle: 'hidden', //esconder barra de título e menu
             autoHideMenuBar: true, //esconder o menu
-            icon: './src/public/img/estoquesobre.png'
+            icon: './src/public/img/ajuda.png'
         })
     }
     // nativeTheme.themeSource = 'dark'
@@ -44,28 +44,98 @@ const aboutWindow = () => {
         about = null
     })
 }
+// janela fornecedor
+let supp //Resolver bug de abertura de várias janelas
+
+const suppWindow = () => {
+    // Se a janela about n estiver aberta (bug 1) abrir
+    if (!supp) {
+        supp = new BrowserWindow({
+            width: 560, //largura
+            height: 400, //altura
+            resizable: false, //evitar o redimensionamento
+            //titleBarStyle: 'hidden', //esconder barra de título e menu
+            autoHideMenuBar: true, //esconder o menu
+            icon: './src/public/img/ajuda.png'
+        })
+    }
+    // nativeTheme.themeSource = 'dark'
+    supp.loadFile('./src/views/fornecedores.html')
+
+    // bug 2 (reabrir a janela ao se estiver fechada)
+    supp.on('closed', () => {
+        supp = null
+    })
+}
+// janela clientes 
+let client //Resolver bug de abertura de várias janelas
+
+const clientWindow = () => {
+    // Se a janela about n estiver aberta (bug 1) abrir
+    if (!client) {
+        client = new BrowserWindow({
+            width: 560, //largura
+            height: 400, //altura
+            resizable: false, //evitar o redimensionamento
+            //titleBarStyle: 'hidden', //esconder barra de título e menu
+            autoHideMenuBar: true, //esconder o menu
+            icon: './src/public/img/ajuda.png'
+        })
+    }
+    // nativeTheme.themeSource = 'dark'
+    client.loadFile('./src/views/clientes.html')
+
+    // bug 2 (reabrir a janela ao se estiver fechada)
+    client.on('closed', () => {
+        client = null
+    })
+}
+
+// janela produtos
+let produt //Resolver bug de abertura de várias janelas
+
+const produtWindow = () => {
+    // Se a janela about n estiver aberta (bug 1) abrir
+    if (!produt) {
+        produt = new BrowserWindow({
+            width: 560, //largura
+            height: 400, //altura
+            resizable: false, //evitar o redimensionamento
+            //titleBarStyle: 'hidden', //esconder barra de título e menu
+            autoHideMenuBar: true, //esconder o menu
+            icon: './src/public/img/ajuda.png'
+        })
+    }
+    // nativeTheme.themeSource = 'dark'
+    produt.loadFile('./src/views/produtos.html')
+
+    // bug 2 (reabrir a janela ao se estiver fechada)
+    produt.on('closed', () => {
+        produt = null
+    })
+}
 // Iniciar a aplicação
 app.whenReady().then(() => {
     createWindow()
- 
+
     //   status de conexão com o banco de dados
     ipcMain.on('send-message', (event, message) => {
         console.log(`<<< ${message} >>>`)
         statusConexao()
     })
- 
+
     // Desconectar do banco ao encerrar a janela
     app.on('before-quit', async () => {
         await desconectar()
     })
- 
+
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
             createWindow()
         }
     })
 })
- 
+
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit()
@@ -82,10 +152,22 @@ const template = [
                 label: 'Sair',
                 click: () => app.quit(),
                 accelerator: 'Alt+F4'
+            },
+            {
+                label: 'Clientes',
+                click: () => clientWindow()
+            },
+            {
+                label: 'Fornecedores',
+                click: () => suppWindow()
+            },
+            {
+                label: 'Produtos',
+                click: () => produtWindow()
             }
         ]
     },
-   
+
     {
         label: 'Ajuda',
         submenu: [
@@ -95,6 +177,33 @@ const template = [
             }
         ]
     },
+
+    {
+        label: 'Exibir',
+        submenu: [
+            {
+                label: 'Recarregar',
+                role: 'reload'
+            },
+            {
+                label: 'Ferramentas',
+                role: 'toggleDevTools'
+            },
+            {
+                label: 'Aplicar zoom',
+                role: 'zoomIn'
+            },
+            {
+                label: 'Reduzir',
+                role: 'zoomOut'
+            },
+            {
+                label: 'Restaurar o zoom padrão',
+                role: 'resetZoom'
+            }
+
+        ]
+    }
 ]
 
 app.on('window-all-closed', () => {
@@ -103,11 +212,7 @@ app.on('window-all-closed', () => {
     }
 })
 
-ipcMain.on('open-about', () => {
-    aboutWindow()
-})
 
- 
 //-------------------------------------------------------------
 // Função para verificar status de conexão com o banco de dados
 const statusConexao = async () => {
--- preload.js
+++ preload.js
@@ -6,4 +6,23 @@ ipcRenderer.send('send-message', "Status do banco de dados:")
 
 ipcRenderer.on('db-status', (event, status) => {
     console.log(status)
+
+    // Inserir data na página
+function obterData() {
+    const data = new Date()
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day:'numeric'
+    }
+    return data.toLocaleDateString('pt-br', options)
+}
+
+// interagir diretamente no DOM do documento html (index.html)
+window.addEventListener('DOMContentLoaded', () => {
+    const dataAtual = document.getElementById('dataAtual').innerHTML = obterData()
+})
+
+
 })
